af48c434d66ef950257ec8e782f73af1d3d4b5dc
alperyaman34 <alperyaman34@hotmail.com>
2026-07-12 00:15:26 +0300

    feat: finalize low-latency networking, clean build workflow, and sort usom threat feed

--- scripts/optimize.sh
+++ scripts/optimize.sh
@@ -11,9 +11,9 @@ find . -name "*.rej" -delete
 chmod +x ../scripts/*.sh
 
 # Ensure custom files are executable
-[ -f ./files/etc/init.d/zapret ] && chmod +x ./files/etc/init.d/zapret
-[ -f ./files/usr/bin/hagezi-guncelle ] && chmod +x ./files/usr/bin/hagezi-guncelle
-[ -f ./files/usr/bin/siteekle ] && chmod +x ./files/usr/bin/siteekle
+[ -d ./files/etc/init.d ] && find ./files/etc/init.d -type f -exec chmod +x {} +
+[ -d ./files/etc/uci-defaults ] && find ./files/etc/uci-defaults -type f -exec chmod +x {} +
+[ -d ./files/usr/bin ] && find ./files/usr/bin -type f -exec chmod +x {} +
 [ -f ./files/opt/zapret/nfqws ] && chmod +x ./files/opt/zapret/nfqws
 
 echo "==> Optimization complete."
